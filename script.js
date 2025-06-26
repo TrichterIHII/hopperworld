bb4d1c7d15de21294608a8801a9f3a08af4bbf8a
TrichterIHII <trichterih@gmail.com>
2025-06-26 21:53:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function(){
   const button_galarie = document.getElementById("galarie");
   const button_updates = document.getElementById("updates");
 
-  button_galerie.addEventListener("click", () => goTo("galarie"));
+  button_galarie.addEventListener("click", () => goTo("galarie"));
   button_updates.addEventListener("click", () => goTo("updates"));
 
   function goTo(place) {
