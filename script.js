2edc0dc325d02901a4ef23cbaee8ab6af213e342
TrichterIHII <trichterih@gmail.com>
2025-06-26 21:24:47 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function(){
+  const button_galarie = document.getElementById("galarie");
+  const button_updates = document.getElementById("updates");
+
+  button_galerie.addEventListener("click", () => goTo("galarie"));
+  button_updates.addEventListener("click", () => goTo("updates"));
+});
+
+function goTo(place) {
+  if (place === "galarie") {
+    
+  }
+  if (place === "updates") {
+    
+  }
+}
