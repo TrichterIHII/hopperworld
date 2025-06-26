4bc2360d812dec78bf9aff2d3220c1d214017922
TrichterIHII <trichterih@gmail.com>
2025-06-26 21:47:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ document.addEventListener("DOMContentLoaded", function(){
 
   button_galerie.addEventListener("click", () => goTo("galarie"));
   button_updates.addEventListener("click", () => goTo("updates"));
-});
 
-function goTo(place) {
-  if (place === "galarie") {
+  function goTo(place) {
+    if (place === "galarie") {
     document.getElementById("container_galarie").scrollIntoView({ behavior: "smooth" });
-  }
-  if (place === "updates") {
+    }
+    if (place === "updates") {
     document.getElementById("container_updates").scrollIntoView({ behavior: "smooth" });
+    }
   }
-}
+});
